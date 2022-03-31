0045ee288f9010745773eb4780631b2a9cd2bd3d
Anton Krasheninnikov <anton.krasheninnikov@clearscale.com>
2022-04-01 00:24:52 +0200

    Adding CLoud support.

--- financial-service-b/src/main/java/ServiceBSprintBootApplication.java
+++ financial-service-b/src/main/java/ServiceBSprintBootApplication.java
@@ -1,5 +1,3 @@
-package io.kadmos.financialtransactionsgateway.application;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -12,19 +10,17 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "io.kadmos.financialtransactionsgateway.controllers")
-public class FinancialTransactionsGatewayApplication {
+public class ServiceBSprintBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FinancialTransactionsGatewayApplication.class, args);
+        SpringApplication.run(ServiceBSprintBootApplication.class, args);
     }
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder
                 .routes()
-                .route(p -> p.path("/savings/a/balance").filters(f -> f.rewritePath("/savings/a/balance", "/balance")).uri("http://localhost:8081"))
-                .route(p -> p.path("/savings/b/balance").filters(f -> f.rewritePath("/savings/b/balance", "/balance")).uri("http://localhost:8082"))
-                .build();
+                .route(p -> p.path("/savings/a/balance").filters(f -> f.rewritePath("/savings/a/balance", "/balance")).uri("http://localhost:8080")).build();
     }
 
 }
