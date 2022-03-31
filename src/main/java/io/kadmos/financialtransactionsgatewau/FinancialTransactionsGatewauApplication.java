14a76acd6c572e6ee2e384695022a4a39315a648
Anton Krasheninnikov <anton.krasheninnikov@clearscale.com>
2022-03-31 22:07:36 +0200

    Init a repository.

---
+++ src/main/java/io/kadmos/financialtransactionsgatewau/FinancialTransactionsGatewauApplication.java
@@ -0,0 +1,13 @@
+package io.kadmos.financialtransactionsgatewau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTransactionsGatewauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTransactionsGatewauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kadmos/financialtransactionsgatewau/FinancialTransactionsGatewauApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kadmos.financialtransactionsgatewau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTransactionsGatewauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
